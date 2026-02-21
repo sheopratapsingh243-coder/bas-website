194ed2306f2cc987169cbd954472fcfb01f4a46f
sheopratapsingh243-coder <sheopratapsingh243@gmail.com>
2026-02-21 20:18:50 +0530

    Update header.js

--- header.js
+++ header.js
@@ -2,5 +2,12 @@ fetch("components/header.html")
 .then(res=>res.text())
 .then(data=>{
 document.getElementById("header").innerHTML=data;
+
+/* wait until header buttons exist */
+setTimeout(()=>{
+if(typeof initLang==="function"){
 initLang();
+}
+},100);
+
 });
